5f796f22ad4346ba2bf5fcfa1bf6f9fc02953707
Insaram Alam Siddique <alaminsaram92@gmail.com>
2025-05-31 00:47:09 +0500

    removed type errors

--- src/common/components/sidebar/app-sidebar.tsx
+++ src/common/components/sidebar/app-sidebar.tsx
@@ -20,9 +20,6 @@ import { useState } from 'react';
 import { usePathname } from 'next/navigation';
 
 export function AppSidebar() {
-  const [isOpen, setIsOpen] = useState<
-    'Custom Property' | 'Template Settings' | ''
-  >('');
   const pathname = usePathname();
   console.log(pathname);
 
@@ -45,7 +42,10 @@ export function AppSidebar() {
           <SidebarGroupContent className='px-6 mt-4 '>
             <SidebarMenu className='space-y-4'>
               {sidebarItems.map(item => (
-                <SidebarMenuItem className='mt-3' key={item.title}>
+                <SidebarMenuItem
+                  className='mt-3'
+                  key={item.title}
+                >
                   <SidebarMenuButton
                     className={cn(
                       'hover:bg-[#FFF3F0] hover:text-primary rounded-[53px]',
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,18 @@
 // import type { NextRequest } from "next/server";
-import { isTokenExpired } from '@/lib/checkTokenExpiry';
-import { NextRequest, NextResponse } from 'next/server';
+// import { isTokenExpired } from '@/lib/checkTokenExpiry';
+import { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get('accessToken')?.value;
+  // const token = req.cookies.get('accessToken')?.value;
 
-  const { pathname } = req.nextUrl.clone();
+  // const { pathname } = req.nextUrl.clone();
 
-  const publicRoutes = [
-    '/login',
-    '/forgot-password',
-    '/otp-verification',
-    '/create-password',
-  ];
+  // const publicRoutes = [
+  //   '/login',
+  //   '/forgot-password',
+  //   '/otp-verification',
+  //   '/create-password',
+  // ];
 
   // if (!token && !publicRoutes.includes(pathname)) {
   //   return NextResponse.redirect(new URL('/login', req.url));
