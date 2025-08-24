5708de0d574bf51d0fd009d3daf7f8f5f83e2d29
daniyalaamir110 <daniyal.amir110@gmail.com>
2025-08-24 18:40:04 +0500

    fix: Dashboard Fixes - Moved the customers API request states to context for global access and utilized via hook. - Replaced hardcoded data from customer map with that from customer API. - Improved customer map UI and added responsive tabs for daily, weekly, and monthly stats. - Fixed name in customers list component. - Fixed duplicates in most ordered dishes panel. Used the first 5 cuisines' data instead for tags and used the rest for other tags.

--- src/app/(dashboard)/dashboard/_components/MostOrderedDishes.tsx
+++ src/app/(dashboard)/dashboard/_components/MostOrderedDishes.tsx
@@ -27,7 +27,16 @@ const MostOrderedDishes = () => {
     getMenuInsight()
   }, []);
 
-  const tags = ['#japanessefood','#design','#projectmanagement','16+']
+  const totalOrders = dishesData?.mostOrderedDishes?.reduce((acc:any, curr:any) => acc + curr.orders, 0)
+
+  const tags = dishesData?.mostPopularCuisines
+    ?.filter((item:any) => item.cuisine && item.orders > 0)
+
+  const topTags = tags.slice(0,5)
+  const otherTags = tags.slice(5)
+
+  if (tags.length === 0) return null;
+
   return (
     <Card>
       <CardHeader>
@@ -35,23 +44,25 @@ const MostOrderedDishes = () => {
         <p className="text-sm text-muted-foreground">Lorem ipsum dolor sit amet, consectetur</p>
       </CardHeader>
       <CardContent className="space-y-4">
-        {dishesData?.mostOrderedDishes?.slice(0,5).map((dish:any) => (
+        {topTags.map((dish:any) => (
           <div key={dish?.itemName || 'NA'} className="space-y-2">
-            <Progress value={dish?.orders || 0} className="h-2" />
+            <Progress value={(dish?.orders / totalOrders) * 100 || 0} className="h-2" />
             <div className="flex justify-between items-center">
-              {dish?.cuisineStyle && <span className="text-sm font-medium">#{dish.cuisineStyle}</span>}
+              <span className="text-sm font-medium">#{dish.cuisine}</span>
               <span className="text-xs text-muted-foreground">{dish.orders} times</span>
             </div>
           </div>
         ))}
-        {/* <p className="text-lg">Other Tags</p> */}
-        {/* <div className="flex flex-wrap gap-3">
+        {otherTags.length > 0 && <>
+          <p className="text-lg">Other Tags</p>
+          <div className="flex flex-wrap gap-3">
             {
-                tags?.map((tag,i) => (
-                    <p key={i} className="text-base w-fit text-red-400 border border-red-400 rounded-3xl px-3 py-2">{tag}</p>
-                ))
+              otherTags?.map((tag:any,i:any) => (
+                <p key={i} className="text-base w-fit text-red-400 border border-red-400 rounded-3xl px-3 py-2">{tag.cuisine}</p>
+              ))
             }
-        </div> */}
+          </div>
+        </>}
       </CardContent>
     </Card>
   );
